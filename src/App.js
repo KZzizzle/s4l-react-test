1c2009eff30fce1b5d94a61d7d8d9e49f3735fc1
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2019-11-05 13:50:06 +0100

    Toggle dark mode and sample buttons

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import React from 'react';
 import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { IconButton, Box, Tooltip } from '@material-ui/core';
+import { Brightness7, Brightness4 } from '@material-ui/icons';
 
 import ToolMenu from './tool/ToolMenu'
 
-const theme = createMuiTheme({
-  palette: {
-    type: 'dark'
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: {
+        palette: {
+          type: 'dark'
+        }
+      }
+    }
   }
-});
-
-function App() {
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <ThemeProvider theme={theme}/>
-        <Container className="App">
-          <ToolMenu />
-        </Container>
-      <ThemeProvider />
-    </React.Fragment>
-  );
+  render() {
+    return (
+      <ThemeProvider theme={createMuiTheme(this.state.theme)}>
+        <CssBaseline />
+        <div className="app"></div>
+        <ToolMenu className="tool-menu" />
+        <Box className="dark-mode-toggle-container">
+          <Tooltip title="Toggle dark mode on/off">
+            <IconButton onClick={this.toggleDarkMode}>
+              {this.state.theme.palette.type === 'dark' ? (
+                <Brightness7 />
+              ) : (
+                <Brightness4 />
+              )}
+            </IconButton> 
+          </Tooltip>
+        </Box>
+      </ThemeProvider>
+    );
+  }
+  toggleDarkMode = () => {
+    this.setState(state => ({
+      theme: {
+        palette: {
+          type: state.theme.palette.type === 'dark' ? 'light' : 'dark'
+        }
+      }
+    }))
+  }
 }
 
 export default App;
--- src/tool/ToolMenu.js
+++ src/tool/ToolMenu.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
+import './ToolMenu.css';
+import { Stop, ChangeHistory, FiberManualRecord, Timeline } from '@material-ui/icons';
+import { IconButton, Box, Tooltip } from '@material-ui/core';
 
-function ToolMenu() {
+function ToolMenu(props) {
   return (
-    <div>
-      <Container>
-        <p>Hello</p>
-      </Container>
-    </div>
+    <Box className={props.className}>
+      <Tooltip title='Prism'>
+        <IconButton>
+          <Stop />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title='Pyramid of Egypt'>
+        <IconButton>
+          <ChangeHistory />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title='Sphere'>
+        <IconButton>
+          <FiberManualRecord />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title='Spline'>
+        <IconButton>
+          <Timeline />
+        </IconButton>
+      </Tooltip>
+    </Box>
   );
 }
 
