f77876940105b3547eb32f7968a1845b135d23bb
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2019-11-05 09:56:47 +0100

    2

---
+++ src/tool/ToolMenu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+
+function ToolMenu() {
+  return (
+    <div>
+      <Container>
+        <p>Hello</p>
+      </Container>
+    </div>
+  );
+}
+
+export default ToolMenu;
