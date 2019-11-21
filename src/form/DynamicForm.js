f19ab1b774b84229ef73dd94787502df79248ab4
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2019-11-21 16:04:03 +0100

    First more or less complete form rendering

--- src/form/DynamicForm.js
+++ src/form/DynamicForm.js
@@ -1,28 +1,69 @@
 import React from 'react';
-import { TextField, Container } from '@material-ui/core';
+import { TextField, Container, FormControlLabel, Checkbox, FormGroup, FormLabel } from '@material-ui/core';
 
 
 const InputElement = props => {
+  const strPath = stringPath(props.path);
   switch (props.type) {
+    case 'boolean':
+      return (
+        <FormControlLabel
+          control={
+            <Checkbox value={strPath} />
+          }
+          label={props.title}
+        />
+      )
+    case 'array':
+      return <ArrayField {...props} />;
     default:
       return (
-        <TextField 
-          id={props.title}
-          label={props.title}
+        <TextField
+          id={strPath}
+          label={props.title || props.givenTitle}
           variant='filled'
           margin='dense'
-          autoComplete='off'
         />
       );
   }
+}
+
+const ArrayField = props => {
+  return (
+    <div>
+      { props.title && props.title.length &&
+        <FormLabel>{props.title}</FormLabel>
+      }
+      <FormGroup row>
+        is array
+      </FormGroup>
+    </div>
+  )
+}
+
+const ValueUnitField = props => {
+  const { path, properties: { value, unit }, title } = props;
+  const valuePath = [...path, 'value'];
+  const unitPath = [...path, 'unit'];
+  return (
+    <div>
+      <FormLabel>{title}</FormLabel>
+      <FormGroup row>
+        <InputElement key={stringPath(valuePath)} {...value} path={valuePath} givenTitle='Value' />
+        <InputElement key={stringPath(unitPath)} {...unit} path={unitPath} givenTitle='Unit' />
+      </FormGroup>
+    </div>
+  )
 }
 
 class DynamicForm extends React.Component {
   render() {
     return (
       <Container>
-        <form>
-          {this.expand(this.props.schema)}
+        <form autoComplete='off'>
+          <FormGroup>
+            {this.expand(this.props.schema)}
+          </FormGroup>
         </form>
       </Container>
     )
@@ -30,13 +71,23 @@ class DynamicForm extends React.Component {
   expand(element, path=[]) {
     return Object.keys(element).map(key => {
       const current = element[key];
+      const newPath = [...path, key];
       if (current.type ==='object') {
-        return this.expand(current.properties, [...path, key])
+        if (isValueUnit(current)) {
+          return <ValueUnitField key={stringPath(newPath)} {...current} path={newPath} />
+        }
+        return this.expand(current.properties, newPath)
       }
-      console.log(`${path.join('.')}.${key}`);
-      return <InputElement key={`${path.join('.')}.${key}`} {...current} />
+      return <InputElement key={stringPath(newPath)} {...current} path={newPath} />
     })
   }
+}
+
+// Util functions
+const stringPath = arrayPath => arrayPath.join('.')
+const isValueUnit = object => {
+  const keys = Object.keys(object.properties);
+  return keys.length === 2 && keys.includes('value') && keys.includes('unit');
 }
 
 export default DynamicForm;
