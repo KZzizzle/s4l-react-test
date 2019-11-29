d37e7d01c446b9ec65d0f6f2c95ab3a826368750
KZzizzle <silunifire@gmail.com>
2019-11-29 16:02:23 +0100

    Created hardcoded tool array and functionality in ToolMenu to read from an array and render appropriate icons

---
+++ src/tool/Tool.js
@@ -0,0 +1,39 @@
+// Hard coding some tools as examples
+
+const Tool0 = {
+    id: 'tool0',
+    label: 'triangle',
+    tooltip: 'pyramid of Egypt'
+};
+
+const Tool1 = {
+    id: 'tool1',
+    label: 'square',
+    tooltip: 'Prism'
+};
+
+const Tool2 = {
+    id: 'tool2',
+    label: 'circle',
+    tooltip: 'Circle'
+};
+
+const Tool3 = {
+    id: 'tool3',
+    label: 'squiggle',
+    tooltip: 'Spline'
+};
+
+const ToolCategory0 = {
+    name: 'toolcategory0',
+    tools: [
+        Tool0,
+        Tool1
+    ]
+};
+
+export const Tools = [
+    Tool2,
+    Tool3,
+    ToolCategory0
+];
--- src/tool/ToolMenu.js
+++ src/tool/ToolMenu.js
@@ -1,31 +1,66 @@
 import React from 'react';
 import './ToolMenu.css';
 import { Stop, ChangeHistory, FiberManualRecord, Timeline } from '@material-ui/icons';
-import { IconButton, Box, Tooltip } from '@material-ui/core';
+import { IconButton, Box, Tooltip, Select, MenuItem } from '@material-ui/core';
+import { Tools } from './Tool'
+
+const AppropriateIcon = props => {
+  switch (props.toolName) {
+    case 'triangle':
+      return <ChangeHistory />;
+    case 'circle':
+      return <FiberManualRecord />;
+    case 'square':
+      return <Stop />;
+    case 'squiggle':
+      return <Timeline />
+    default:
+      return <ChangeHistory /> 
+  }
+};
+
+const MakeTool = props => {
+  return (
+    <Tooltip title = {props.tool.tooltip}>
+      <IconButton>
+        <AppropriateIcon toolName = {props.tool.label} />
+      </IconButton>
+    </Tooltip>
+  )
+}
+
+const MakeToolCategory = props => {
+  return (
+    <Tooltip>
+      <Select>
+        <AppropriateIcon toolName = {props.category.name} />
+        {
+          props.category.tools.map(tool => {
+          return(
+            <MenuItem>
+              <MakeTool tool = {tool} />
+            </MenuItem>
+          )
+        })}
+      </Select>
+    </Tooltip>
+  )
+}
 
 function ToolMenu(props) {
+  const numtools = Tools.length;
+  console.log(Tools.length)
   return (
     <Box className={props.className}>
-      <Tooltip title='Prism'>
-        <IconButton>
-          <Stop />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title='Pyramid of Egypt'>
-        <IconButton>
-          <ChangeHistory />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title='Sphere'>
-        <IconButton>
-          <FiberManualRecord />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title='Spline'>
-        <IconButton>
-          <Timeline />
-        </IconButton>
-      </Tooltip>
+      { Tools.map(entry => {
+          if (entry.name ==undefined) {
+            return(<MakeTool tool = {entry} />)
+          }
+          else {
+            return(<MakeToolCategory category = {entry} />)
+          }
+        })
+      }
     </Box>
   );
 }
